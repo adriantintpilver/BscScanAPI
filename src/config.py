c6d2916b917daf641bdb74740161f29602153e81
adriantintpilver <adriantintpilver@gmail.com>
2023-01-31 18:30:15 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ YOUR_API_KEY = "X74KBRFD1MUMV265BRBM4FRQ4K6UJ614GE"
 
 class DevelopmentConfig():
     DEBUG = True
-    MYSQL_HOST = '172.19.0.1'
+    MYSQL_HOST = '172.19.0.2'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'BscScanAPI_PasSWoRd'
     MYSQL_DB = 'BscScanAPI_DB'
